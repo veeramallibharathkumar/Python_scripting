05f5052091ff19027a0c472c76947a677c3cff4e
bharathcr77 <veeramallibharath@gmail.com>
2024-04-25 22:33:32 -0500

    initial commit

---
+++ source/variablesandstrings.py
@@ -0,0 +1,10 @@
+#concatenation(combing strings)
+
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+print(full_name)
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+print(full_name.strip()) ## By default, strip() removes whitespace characters such as spaces, tabs, and newlines.
